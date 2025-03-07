4e0aebf311bd9a6fc4e02f045fd86c7ae78ed9fa
tlgusdl03 <tlgusdl03@gmail.com>
2025-03-07 10:50:33 +0900

    fix : curItem이 undefinded인 경우 생기는 문제 해결결

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -2,15 +2,28 @@ import { useParams, useNavigate } from "react-router-dom";
 import useItem from "../hooks/useItem";
 import Header from "../components/header/Header";
 import Viewer from "../components/body/Viewer";
+import { useEffect, useState } from "react";
 const Item = () => {
   const params = useParams();
   const nav = useNavigate();
   const curItem = useItem(params.id);
   console.log(curItem);
+  const [loading, setLoading] = useState(true);
 
-  if (!curItem) {
+  useEffect(() => {
+    if (curItem !== undefined) {
+      setLoading(false);
+    }
+  }, [curItem]);
+
+  if (loading) {
     return <div>데이터 로딩중...!</div>;
   }
+
+  if (!curItem) {
+    return <div>존재하지 않는 아이템입니다.</div>;
+  }
+
   return (
     <div>
       <Header />
